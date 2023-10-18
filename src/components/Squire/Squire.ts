759e1dc3ae70ddaa6fe1c1fcb8f2a0ba0e4b31d4
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-18 02:42:55 +0200

    Add Squire class

---
+++ src/components/Squire/Squire.ts
@@ -0,0 +1,41 @@
+import Character from "../Character/Character";
+import { Fighter } from "../Fighter/Fighter";
+import type { CharacterCast } from "../characterInterfaces/CharacterCast";
+
+export class Squire extends Character {
+  assKissingLevel: number;
+  serves: string;
+
+  constructor(
+    characterCast: CharacterCast,
+    assKissingLevel: number,
+    serves: Fighter,
+  ) {
+    super(characterCast);
+    this.assKissingLevel = this.assKissingFilter(assKissingLevel);
+
+    if (!(serves instanceof Fighter)) {
+      throw new Error("A squire can only serve a Fighter.");
+    }
+
+    if (serves instanceof Fighter) {
+      this.serves = serves;
+    }
+  }
+
+  communicate() {
+    return `${super.communicate()} I'm a loser :(`;
+  }
+
+  private assKissingFilter(assKissingLevel: number) {
+    if (assKissingLevel < 0) {
+      return 0;
+    }
+
+    if (assKissingLevel > 10) {
+      return 10;
+    }
+
+    return assKissingLevel;
+  }
+}
