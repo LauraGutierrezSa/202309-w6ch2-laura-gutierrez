aea0f777c00dfd1bceb526d0fe2bd5c678039e34
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-18 02:54:57 +0200

    Use --fix command to pass code audit

--- public/js/components/Squire/Squire.js
+++ public/js/components/Squire/Squire.js
@@ -10,17 +10,21 @@ export class Squire extends Character {
       throw new Error("A squire can only serve a Fighter.");
     }
   }
+
   communicate() {
     return `${super.communicate()} I'm a loser :(`;
   }
+
   assKissingFilter(assKissingLevel) {
     if (assKissingLevel < 0) {
       return 0;
     }
+
     if (assKissingLevel > 10) {
       return 10;
     }
+
     return assKissingLevel;
   }
 }
-//# sourceMappingURL=Squire.js.map
+// # sourceMappingURL=Squire.js.map
